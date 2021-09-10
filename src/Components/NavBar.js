cdf4f09a300c1462688e0994fc23e738da54c75f
Luciano Giraudi <lucianoagiraudi@gmail.com>
2021-09-10 15:49:18 -0300

    cambios en NavBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,34 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav';
+
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import { Link } from 'react-router-dom';
 
 
 
 export default function NavBar() {
+    const [activeHome, setActiveHome] = useState("");
+    const [activeBio, setActiveBio] = useState("");
+    const [activeAna, setActiveAna] = useState("");
+    const [activeDig, setActiveDig] = useState("");
+    const [activeContact, setActiveContact] = useState("");
+    const activatedHome = () => {
+        setActiveHome("activeNav")
+        setActiveBio("")
+        setActiveAna("")
+        setActiveDig("")
+        setActiveContact("")
+    }
+    const activatedBio = () => {
+        setActiveHome("")
+        setActiveBio("activeNav")
+        setActiveAna("")
+        setActiveDig("")
+        setActiveContact("")
+    }
+    const activatedAna = () => {
+        setActiveHome("")
+        setActiveBio("")
+        setActiveAna("activeDrop")
+        setActiveDig("")
+        setActiveContact("")
+    }
+    const activatedDig = () => {
+        setActiveHome("")
+        setActiveBio("")
+        setActiveAna("")
+        setActiveDig("activeDrop")
+        setActiveContact("")
+    }
+    const activatedContact = () => {
+        setActiveHome("")
+        setActiveBio("")
+        setActiveAna("")
+        setActiveDig("")
+        setActiveContact("activeNav")
+    }
 
+    const activeDropAn = () =>{
+        setActiveAna("activeDrop")
+    }
+    const activeDropDig = () =>{
+        setActiveDig("activeDrop")
+    }
     return (
         <Navbar collapseOnSelect expand="lg" id="navBar">
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-                <Link to={`/home`} >Home</Link>
-                <Link to={`/bio`} >Bio</Link>
-                <NavDropdown title="Analogicas" id="collasible-nav-dropdown">
-                <Link to={``} >Retratos</Link>
-                <Link to={``} >Naturaleza</Link>
-                <Link to={``} >Doble Exposicion</Link>
-                <Link to={``} >Arquitectura</Link>
-                <Link to={``} >Callejera</Link>
+                <Link to={`/home`} className={activeHome} onClick={activatedHome}>Home</Link>
+                <Link to={`/bio`} className={activeBio} onClick={activatedBio}>Bio</Link>
+                <NavDropdown title="Analogicas" id="collasible-nav-dropdown" className={activeAna} onClick={activeDropAn}>
+                    <Link to={``} onClick={activatedAna}>Retratos</Link>
+                    <Link to={``} onClick={activatedAna}>Naturaleza</Link>
+                    <Link to={``} onClick={activatedAna}>Doble Exposicion</Link>
+                    <Link to={``} onClick={activatedAna}>Arquitectura</Link>
+                    <Link to={``} onClick={activatedAna}>Callejera</Link>
                 </NavDropdown>
-                <NavDropdown title="Digitales" id="collasible-nav-dropdown">
-                <Link to={``} >Retratos</Link>
-                <Link to={``} >Naturaleza</Link>
-                <Link to={``} >Doble Exposicion</Link>
-                <Link to={``} >Arquitectura</Link>
-                <Link to={``} >Callejera</Link>
+                <NavDropdown title="Digitales" id="collasible-nav-dropdown" className={activeDig}  onClick={activeDropDig}>
+                    <Link to={``} onClick={activatedDig}>Retratos</Link>
+                    <Link to={``} onClick={activatedDig}>Naturaleza</Link>
+                    <Link to={``} onClick={activatedDig}>Doble Exposicion</Link>
+                    <Link to={``} onClick={activatedDig}>Arquitectura</Link>
+                    <Link to={``} onClick={activatedDig}>Callejera</Link>
                 </NavDropdown>
-                <Link to={`/contact`} >Contact</Link>
+                <Link to={`/contact`} className={activeContact} onClick={activatedContact} >Contact</Link>
             </Navbar.Collapse>
         </Navbar>
     )
